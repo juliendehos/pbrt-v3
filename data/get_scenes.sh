94dee713511f72fff34fa9d905b95daa13cdc888
Julien Dehos <dehos@lisic.univ-littoral.fr>
2016-04-13 20:37:59 +0200

    modif get_scenes.sh

--- data/get_scenes.sh
+++ data/get_scenes.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
-curl -L https://mmp.storage.googleapis.com/pbrt/pbrt-v3-scenes.tar.bz2 | tar jx
+curl -L https://mmp.storage.googleapis.com/pbrt/pbrt-v3-scenes.tar.bz2 -o pbrt-v3-scenes.tar.bz2
+
+tar -jxf pbrt-v3-scenes.tar.bz2
 
