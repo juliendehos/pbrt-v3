3564ad9454e960e9c8f319ecda841da0355e0a7a
Julien Dehos <dehos@lisic.univ-littoral.fr>
2016-04-15 16:59:42 +0200

    test scalability

---
+++ data/veach/run_threads.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+#OAR -l /nodes=1/core=32,walltime=50:00:00
+#OAR -t besteffort
+
+NTHREADS="1 2 4 8 16 32"
+SCENES=$(find . -name "*.pbrt")
+OUTPUT="output.csv"
+
+rm -f $OUTPUT
+
+for scene in $SCENES ; do
+    for nthread in $NTHREADS ; do
+        t=$(../../build/pbrt $scene "Rendering " | awk '{print $3}')
+        echo "${scene}; ${nthread}; ${t}" >> $OUTPUT
+    done
+done
+
