257c42e2e4044825d6fb2439d3f8d101ab89ebd2
Julien Dehos <dehos@lisic.univ-littoral.fr>
2016-04-15 15:16:01 +0200

    veach scene

--- data/get_scenes.sh
+++ data/get_scenes.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
+# pbrt-v3-scenes
 curl -L https://mmp.storage.googleapis.com/pbrt/pbrt-v3-scenes.tar.bz2 -o pbrt-v3-scenes.tar.bz2
-
 tar -jxf pbrt-v3-scenes.tar.bz2
+
+# pbrt-v2-scenes
+#curl -L https://s3.amazonaws.com/pbrt/pbrt-scenes.zip -o pbrt-scenes.zip
+#unzip pbrt-scenes.zip
 
