8c2062a0e1b1126d3af2dd65595bddc76e14ebb2
Julien Dehos <dehos@lisic.univ-littoral.fr>
2016-07-21 15:23:54 +0200

    typo

--- data_noise/run_oar.sh
+++ data_noise/run_oar.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 #OAR --array-param-file run_oar.txt
-#OAR -l /nodes=1/core=1,walltime=500:00:00
+#OAR -l /nodes=1/core=1,walltime=5000:00:00
 #OAR -t besteffort
 ##OAR --notify mail:toto@tata.com
 
