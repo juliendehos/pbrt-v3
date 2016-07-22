453a98868ff61537573f99f65eefeb9bdca459a0
Julien Dehos <dehos@lisic.univ-littoral.fr>
2016-07-22 09:32:08 +0200

    don't generate useless rendering config

--- data_noise/generate_params.sh
+++ data_noise/generate_params.sh
@@ -3,19 +3,26 @@
 # ./generate_params.sh > run_oar.txt 
 
 SCENES="breakfast spheres sportscar veach"
-ALGOS="path mlt"
 DEPTHS="1 2 4 32"
 SAMPLES="4 32 256 2048 16384"
 RNGS="sobol halton random lowdiscrepancy"
 
+# algo: path
 for scene in $SCENES; do
-    for algo in $ALGOS; do
-        for depth in $DEPTHS; do
-            for sample in $SAMPLES; do
-                for rng in $RNGS; do
-                    echo $scene $algo $depth $sample $rng
-                done
+    for depth in $DEPTHS; do
+        for sample in $SAMPLES; do
+            for rng in $RNGS; do
+                echo $scene path $depth $sample $rng
             done
+        done
+    done
+done
+
+# algo: mlt
+for scene in $SCENES; do
+    for depth in $DEPTHS; do
+        for sample in $SAMPLES; do
+            echo $scene mlt $depth $sample random
         done
     done
 done
