e31ef6a1f1f50ced10c4e6f68e7aaa0b0e6fabc1
Julien Dehos <dehos@univ-littoral.fr>
2016-11-23 11:13:05 +0100

    maj

--- data_noise/run_tonemapping.sh
+++ data_noise/run_tonemapping.sh
@@ -11,6 +11,6 @@ name=${dir_name}/${base_name%.exr}
 
 exrtopng ${name}.exr ${name}.png
 
-pfsinexr ${name}.exr | pfstmo_reinhard05  | pfsoutexr tmp_${base_name}.exr && exrtopng tmp_${base_name}.exr ${name}_reinhard05.png
-rm tmp_${base_name}.exr
+#pfsinexr ${name}.exr | pfstmo_reinhard05  | pfsoutexr tmp_${base_name}.exr && exrtopng tmp_${base_name}.exr ${name}_reinhard05.png
+#rm tmp_${base_name}.exr
 
