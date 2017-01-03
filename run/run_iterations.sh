ec4a939b24b0a5816d97893385cc6681612f9c54
Julien Dehos <dehos@lisic.univ-littoral.fr>
2017-01-03 15:09:41 +0100

    menage script

--- run/run_iterations.sh
+++ run/run_iterations.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-#OAR -l /nodes=1/core=5,walltime=9000:00:00
+#OAR -l /nodes=1,walltime=900:00:00
 #OAR -q long
-##OAR -t besteffort
-##OAR -t idempotent
+#OAR -t besteffort
+#OAR -t idempotent
 #OAR --notify mail:dehos@univ-littoral.fr
 
-../build/pbrt veach/veach_32.pbrt
+../build/pbrt ../data/cornellbox_pointlight/cornellbox_pointlight.pbrt
 
